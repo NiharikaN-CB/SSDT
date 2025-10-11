bb5c1e4b55d4ebfaf936e50580649ebec9f9387b
NiharikaN-CB <niharikaniranjan688@gmail.com>
2025-10-11 19:13:26 +0530

    modified files

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json({ extended: false }));
 app.use('/auth', require('./routes/auth'));
 app.use('/api/vt', virustotalRoutes);
 
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Backend server started on port ${PORT}`));
 
